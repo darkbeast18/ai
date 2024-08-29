2bee3b2e91073a1ef77b0be883ba1ae342d4b32c
darkbeast18 <prudhvichowdary45436@gmail.com>
2024-08-29 20:24:28 +0530

    Update tic-toc-toe.py

--- tic-toc-toe.py
+++ tic-toc-toe.py
@@ -47,18 +47,15 @@ def isWinner(bo, le):
             (bo[9] == le and bo[5] == le and bo[1] == le))   # diagonal
 
 def getBoardCopy(board):
-    # Make a duplicate of the board list and return it the duplicate.
     dupeBoard = []
     for i in board:
         dupeBoard.append(i)
     return dupeBoard
 
 def isSpaceFree(board, move):
-    # Return true if the passed move is free on the passed board.
     return board[move] == ' '
 
 def getPlayerMove(board):
-    # Let the player type in his move.
     move = ' '
     while move not in '1 2 3 4 5 6 7 8 9'.split() or not isSpaceFree(board, int(move)):
         print('What is your next move? (1-9)')
@@ -66,8 +63,6 @@ def getPlayerMove(board):
     return int(move)
 
 def chooseRandomMoveFromList(board, movesList):
-    # Returns a valid move from the passed list on the passed board.
-    # Returns None if there is no valid move.
     possibleMoves = []
     for i in movesList:
         if isSpaceFree(board, i):
@@ -82,33 +77,26 @@ def getComputerMove(board, computerLetter):
         playerLetter = 'O'
     else:
         playerLetter = 'X'
-    # Here is our algorithm for our Tic Tac Toe AI:
-    # First, check if we can win in the next move
     for i in range(1, 10):
         copy = getBoardCopy(board)
         if isSpaceFree(copy, i):
             makeMove(copy, computerLetter, i)
             if isWinner(copy, computerLetter):
                 return i
-    # Check if the player could win on his next move, and block them.
     for i in range(1, 10):
         copy = getBoardCopy(board)
         if isSpaceFree(copy, i):
             makeMove(copy, playerLetter, i)
             if isWinner(copy, playerLetter):
                 return i
-    # Try to take one of the corners, if they are free.
     move = chooseRandomMoveFromList(board, [1, 3, 7, 9])
     if move != None:
         return move
-    # Try to take the center, if it is free.
     if isSpaceFree(board, 5):
         return 5
-    # Move on one of the sides.
     return chooseRandomMoveFromList(board, [2, 4, 6, 8])
 
 def isBoardFull(board):
-    # Return True if every space on the board has been taken. Otherwise return False.
     for i in range(1, 10):
         if isSpaceFree(board, i):
             return False
